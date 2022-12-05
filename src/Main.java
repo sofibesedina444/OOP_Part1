1a6f975c6fa3ef221e10481c0095b858675de1cf
salva <salvationordeath@gmail.com>
2022-12-05 18:54:51 +0700

    ООП. Первая часть ДЗ по уроку №1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,13 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Human person1 = new Human("Максим", "Минск", 1988, "бренд-менеджер");
+        person1.displayHuman("мужчина");
+        Human person2 = new Human("Аня", "Москва", 1993, "методист образовательных программ");
+        person2.displayHuman("женщина");
+        Human person3 = new Human("Катя", "Калининград", 1992, "продакт-менеджер");
+        person3.displayHuman("женщина");
+        Human person4 = new Human("Артем", "Москва", 1995, "директор по развитию бизнеса");
+        person4.displayHuman("мужчина");
     }
 }
