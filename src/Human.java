dcb032d944b8765cf13049d83bee8b54c48b69c1
salva <salvationordeath@gmail.com>
2022-12-08 13:40:26 +0700

    ООП. Первая часть ДЗ по уроку №2

--- src/Human.java
+++ src/Human.java
@@ -1,5 +1,5 @@
 public class Human {
-    private int yearOfBirth;
+    private final int yearOfBirth;
     private final String name;
     private final String town;
     private String jobTitle;
@@ -16,11 +16,7 @@ public class Human {
         } else {
             this.town = "Информация не указана";
         }
-        if (jobTitle != null && !jobTitle.isEmpty()){
-            this.jobTitle = jobTitle;
-        } else {
-            this.jobTitle = "Информация не указана";
-        }
+        setJobTitle(jobTitle);
     }
 
     public void displayHuman(String gender) {
@@ -32,6 +28,30 @@ public class Human {
                     + yearOfBirth + " году. Я работаю на должности " + jobTitle + ". Будем знакомы!");
         } else {
             System.out.println("Неверно указан пол человека!");
+        }
+    }
+
+    public int getYearOfBirth() {
+        return yearOfBirth;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getTown() {
+        return town;
+    }
+
+    public String getJobTitle() {
+        return jobTitle;
+    }
+
+    public void setJobTitle(String jobTitle) {
+        if (jobTitle != null && !jobTitle.isEmpty()){
+            this.jobTitle = jobTitle;
+        } else {
+            this.jobTitle = "Информация не указана";
         }
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-
+        //Задания первого урока по ООП
         Human person1 = new Human("Максим", "Минск", 1988, "бренд-менеджер");
         person1.displayHuman("мужчина");
         Human person2 = new Human("Аня", "Москва", 1993, "методист образовательных программ");
@@ -9,5 +9,38 @@ public class Main {
         person3.displayHuman("женщина");
         Human person4 = new Human("Артем", "Москва", 1995, "директор по развитию бизнеса");
         person4.displayHuman("мужчина");
+        Human person5 = new Human("Владимир", "Казань", 2001, null);
+        person5.displayHuman("мужчина");
+
+        System.out.println("\n");
+
+        Flower flower1 = new Flower("Роза обыкновенная", "Голландия", 35.59);
+        System.out.println(flower1);
+        Flower flower2 = new Flower("Хризантема", "", 15);
+        flower2.lifeSpan = 5;
+        System.out.println(flower2);
+        Flower flower3 = new Flower("Пион", "Англия", 69.9);
+        flower3.lifeSpan = 1;
+        System.out.println(flower3);
+        Flower flower4 = new Flower("Гипсофила", "Турция", 19.5);
+        flower4.lifeSpan = 10;
+        System.out.println(flower4);
+
+        System.out.println("\n");
+        getInfoCostFlowers(flower1, flower1, flower2, flower2, flower3, flower3, flower4, flower4);
+    }
+    public static void getInfoCostFlowers(Flower...flowers) {
+        double totalCost = 0;
+        int minLifeSpanFlowers = Integer.MAX_VALUE;
+        for (Flower flower : flowers) {
+            if (flower.lifeSpan < minLifeSpanFlowers) {
+                minLifeSpanFlowers = flower.lifeSpan;
+            }
+            totalCost += flower.getCost();
+            System.out.println(flower);
+        }
+        totalCost = totalCost * 1.1;
+        System.out.println("Стоимость букета: " + totalCost);
+        System.out.println("Срок состояния букета: " + minLifeSpanFlowers);
     }
 }
